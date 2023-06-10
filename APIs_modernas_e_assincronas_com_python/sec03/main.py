07baa4dc03f72bc12b3e3e4769462b212ae225d6
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-10 15:34:05 -0300

    m

--- APIs_modernas_e_assincronas_com_python/sec03/main.py
+++ APIs_modernas_e_assincronas_com_python/sec03/main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI, HTTPException, status
+from typing import Optional
+
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    status,
+    Response,
+    Path,
+    Query,
+    Header,
+)
 
 from models import Curso
 
@@ -24,7 +34,15 @@ async def get_cursos():
 
 
 @app.get("/cursos/{curso_id}")
-async def get_curso(curso_id: int):
+async def get_curso(
+    curso_id: int = Path(
+        default=None,
+        title="ID do curso",
+        description="Deve ser entre 1 e 2",
+        gt=0,
+        lt=3,
+    )
+):
     try:
         curso = cursos[curso_id]
         return curso
@@ -36,7 +54,49 @@ async def get_curso(curso_id: int):
 async def post_curso(curso: Curso):
     next_id = len(cursos) + 1
     cursos[next_id] = curso
+    del curso.id
     return curso
+
+
+@app.put("/cursos/{curso_id}")
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id  in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Não existe um curso com id {curso_id}",
+        )
+
+
+@app.delete("/cursos/{curso_id}")
+async def delete_curso(curso_id: int):
+    print(curso_id)
+    if curso_id in cursos:
+        del cursos[curso_id]
+        # return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Não existe um curso com id {curso_id}",
+        )
+
+
+@app.get("/calculadora")
+async def calcular(
+    a: int = Query(default=None, gt=5),
+    b: int = Query(default=None, gt=10),
+    c: Optional[int] = None,
+    x_geek: str = Header(default=None),
+):
+    soma = a + b
+    if c:
+        soma += c
+    print(f'X-GEEK: {x_geek}')
+    return {"resultado": soma}
 
 
 if __name__  == "__main__":
