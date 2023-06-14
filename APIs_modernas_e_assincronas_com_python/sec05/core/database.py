f20d6103210ccb3f5eb5755c0bdcf3840c8a8c78
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-14 15:46:04 -0300

    m

--- APIs_modernas_e_assincronas_com_python/sec05/core/database.py
+++ APIs_modernas_e_assincronas_com_python/sec05/core/database.py
@@ -1,11 +1,8 @@
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.asyncio import AsyncEngine
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import Session
-
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
 
 from core.configs import settings
+
 
 engine: AsyncEngine = create_async_engine(settings.DB_URL)
 
