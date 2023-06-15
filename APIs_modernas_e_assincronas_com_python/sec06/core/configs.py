ec21d9c5360a7efcf67b4322c64911abc6ed50e6
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-15 12:56:07 -0300

    m

---
+++ APIs_modernas_e_assincronas_com_python/sec06/core/configs.py
@@ -0,0 +1,24 @@
+from pydantic import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str ="/api/v1"
+    DB_URL: str = "postgresql+asyncpg://geek:university@localhost:5432/faculdade"
+    DBBaseModel = declarative_base
+
+    JWT_SECRET: str = "I20x2huLS999mzyYhj_2E9KWgwhy3JgSAVMvB50QWa4"
+    """
+    import secrets
+
+    token: str = secrets.token_urlsafe(32)
+    """
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+
+    class Config:
+        case_sensitive = True
+
+
+settings: Settings = Settings()
---
+++ APIs_modernas_e_assincronas_com_python/sec06/core/security.py
@@ -0,0 +1,20 @@
+from passlib.context import CryptContext
+
+
+CRIPTO = CryptContext(schemes=["bcrypt"], depecatedd="auto")
+
+
+def verificar_senha(senha: str, hash_senha: str) -> bool:
+    """
+    Função para verificar se a senhaa etáa correta, comporando
+    a senha em texto puro, informando pelo usuário, ee o hash da
+    senha que estará salvo no banco de daados duraante a criação ad conta.
+    """
+    return CRIPTO.verify(senha, hash_senha)
+
+
+def gerar_hash_senha(senha: str) -> str:
+    """
+    Função que geraa e retorna o hash da senha
+    """
+    return CRIPTO.hash(senha)
