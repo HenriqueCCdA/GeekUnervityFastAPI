404e1c2cbb2647e274c812854fa397100b37a2d9
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-06-24 21:59:22 -0300

    m

--- websites_modernos_e_assincronos_com_python/sec04/core/database.py
+++ websites_modernos_e_assincronos_com_python/sec04/core/database.py
@@ -7,26 +7,28 @@ from core.configs import settings
 engine: AsyncEngine = create_async_engine(settings.DB_URL, echo=False)
 
 
+engine: AsyncEngine = create_async_engine(settings.DB_URL, echo=False)
+
+
 def get_session() -> AsyncSession:
     __async_session = sessionmaker(
         autocommit=False,
         autoflush=False,
         expire_on_commit=False,
-        class_ = AsyncSession,
-        bind=engine,
+        class_=AsyncSession,
+        bind=engine
     )
 
     session: AsyncSession = __async_session()
+
 
     return session
 
 
 async def create_tables() -> None:
     import models.__all_models
-    print("Criando as tables no banco de dados")
-
+    print('Criando as tabelas no banco de dados')
     async with engine.begin() as conn:
-        await conn.run_sync(settings.DBBaseModel.metada.drop_all)
-        await conn.run_sync(settings.DBBaseModel.metada.create_all)
-
-    print("Tabelas criadas com sucesso")
+        await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
+        await conn.run_sync(settings.DBBaseModel.metadata.create_all)
+    print('Tabelas criadas com sucesso')
